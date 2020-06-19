750741eda9d6313cd880aa611d031e1b95176226
ilkerkuss <45232369+ilkerkuss@users.noreply.github.com>
2020-06-19 16:45:59 +0300

    Last

--- src/Login.js
+++ src/Login.js
@@ -8,7 +8,7 @@ export default function Login() {
     return (
       <NavigationContainer>
           <Text>
-              Merhaba
+              Merhaba 
           </Text>
       </NavigationContainer>
     );
